7d4ea1ef049fa8c5f83980cd6840b7263a5287b6
陳起 <861124cc@gmail.com>
2023-12-07 00:12:15 +0800

    新增acc功能以及分配url 測試git pull的的commit

--- acc/urls.py
+++ acc/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include #引用外部設定 
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("acc/",include("acc.urls")),
-    path("admin/", admin.site.urls),
+    path("slaes_data",views.slaes_data),
 ]
